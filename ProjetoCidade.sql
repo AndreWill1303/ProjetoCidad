df366017911f219750c83ffa260c44c21650489c
Andre <andre.silva1303@etec.sp.gov.br>
2025-05-19 14:11:24 -0300

    Criando Banco de Dados

---
+++ ProjetoCidade.sql
@@ -0,0 +1,17 @@
+create database dbProjetoCidad;
+use dbProjetoCidad;
+
+create table tbUsuario(
+Id int not null,
+Nome varchar (50)not null,
+Email varchar (50) not null,
+Senha varchar (50) not null
+);
+
+create table tbProdutos (
+Id int not null,
+Nome varchar (50) not null,
+Descricao varchar (50) not null,
+Preco decimal not null,
+Quantidade int
+);
